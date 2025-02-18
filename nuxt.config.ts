6fed1372ad70fda704261c254894254e186371f0
luddthree <luddetv@gmail.com>
2025-02-18 11:33:40 +0100

    update API base URL to production server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   plugins: ['~/plugins/auth.js'],
   runtimeConfig: {
     public: {
-      apiBase: 'http://127.0.0.1:8000' // Laravel API URL
+      apiBase: 'http://ofl.vang.li:8000' // Laravel API URL
     }
   },
   compatibilityDate: '2024-04-03',
